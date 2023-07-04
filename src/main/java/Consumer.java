99d3f2c2199abc7fdbe2603c1e54569d2dbdf385
Vladimir Dubceac <vladimir.dubceac@bass.md>
2023-07-04 22:15:39 +0300

    First commit

---
+++ src/main/java/Consumer.java
@@ -0,0 +1,24 @@
+import org.springframework.http.HttpEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Consumer {
+    public static void main(String[] args) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        Map<String,String> jsonToSend = new HashMap<>();
+        jsonToSend.put("name","Test name");
+        jsonToSend.put("job","Test job");
+
+        HttpEntity<Map<String,String>> request = new HttpEntity<>(jsonToSend);
+
+        String url = "https://reqres.in/api/users/2";
+        String getResponse = restTemplate.getForObject(url, String.class);
+        String postResponse = restTemplate.postForObject(url, request, String.class);
+
+        System.out.println(getResponse);
+        System.out.println(postResponse);
+    }
+}
